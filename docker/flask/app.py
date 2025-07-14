bd2e65071bd654f3719cdb397834cf0873bb63b4
saahil tanwar <saahiltanwarr@gmail.com>
2025-07-14 17:18:54 +0530

    Update app.py

--- docker/flask/app.py
+++ docker/flask/app.py
@@ -8,8 +8,8 @@ REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requ
 @app.route('/')
 @REQUEST_TIME.time()
 def home():
-return "Hello from monitored Flask app!"
+    return "Hello from monitored Flask app!"
 
 if __name__ == '__main__':
-start_http_server(8000) # Prometheus metrics on :8000
-app.run(host='0.0.0.0', port=5000)
+    start_http_server(8000) # Prometheus metrics on :8000
+    app.run(host='0.0.0.0', port=5000)
